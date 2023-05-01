e72f492a461b9872c01a0009c9e1f06701c8bba8
lumi <>
2023-05-01 14:49:52 -0700

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,38 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let input = std::fs::read_to_string(filename).unwrap();
+    println!("{}", a(input));
+
+    let input = std::fs::read_to_string(filename).unwrap();
+    println!("{}", b(input));
+}
+
+fn a(input: String) -> usize {
+    input.split("\n\n").map(|elf| {
+        elf.split("\n").map(|snack| snack.parse::<usize>().unwrap()).sum()
+    }).max().unwrap()
+}
+
+fn b(input: String) -> usize {
+    let mut total_calories_by_elf = input.split("\n\n").map(|elf| {
+        elf.split("\n").map(|snack| snack.parse::<usize>().unwrap()).sum()
+    }).collect::<Vec<usize>>();
+    total_calories_by_elf.sort();
+    total_calories_by_elf.iter().rev().take(3).sum()
+}
+
+#[test]
+fn test_example_a() {
+    let input = std::fs::read_to_string("src/example_data.txt").unwrap();
+    assert_eq!(a(input), 24000);
+}
+
+#[test]
+fn test_example_b() {
+    let input = std::fs::read_to_string("src/example_data.txt").unwrap();
+    assert_eq!(b(input), 45000);
+}
